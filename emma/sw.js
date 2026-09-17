2d6a1855e41f19759b559faca2023780ee80b75b
RickyMeyerFilms <info@rickymeyerfilms.com>
2026-09-16 23:10:36 -0700

    S&S crew app

---
+++ emma/sw.js
@@ -0,0 +1,25 @@
+var C = "ss-emma-09faca131b";
+var FILES = ["./", "./index.html", "./manifest.webmanifest", "./apple-touch-icon.png"];
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(C).then(function (c) { return c.addAll(FILES); })
+    .then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (ks) {
+    return Promise.all(ks.map(function (k) { return k === C ? null : caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(function (r) {
+      var copy = r.clone();
+      caches.open(C).then(function (c) { c.put(e.request, copy); });
+      return r;
+    }).catch(function () {
+      return caches.match(e.request, { ignoreSearch: true }).then(function (r) {
+        return r || caches.match("./index.html");
+      });
+    })
+  );
+});
